164d7803b559b75b5f2f47e61a700e5ebb5d3763
arnavthorat78 <arnavthorat78@gmail.com>
2021-10-17 12:23:30 +1100

    Finished Popup type definitions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,17 +8,17 @@ The Changelog shows all of the different versions that have come and gone, and w
 
 This version introduced better type definitions, and also a new class!
 
--  Better type definitions were introduced ([#2](https://github.com/arnavthorat78/Popup-Prompt/issues/2)).
--  Added a new class (`Popup`). This creates a customizable popup! _Note: This class does not have many features, and is in BETA._
+-   Better type definitions were introduced ([#2](https://github.com/arnavthorat78/Popup-Prompt/issues/2)).
+-   Added a new class (`Popup`). This creates a customizable popup! _Note: This class is in BETA._
 
 ### `v1.0.0`
 
 This version was the starting version. It featured two constants and two methods, listed below.
 
--  `VERSION` (Constant)
--  `AVAILABLE_VALUES` (Constant)
--  `showMessageBox` (Method)
--  `showPrompt` (Method)
+-   `VERSION` (Constant)
+-   `AVAILABLE_VALUES` (Constant)
+-   `showMessageBox` (Method)
+-   `showPrompt` (Method)
 
 ## Propose New Changes/Additions?
 
--- index.d.ts
+++ index.d.ts
@@ -165,6 +165,14 @@ export const showPrompt: (title: string, message: string, defaultValue?: string)
 // Classes //
 ////////////
 
+// Popup types //
+type startPos =
+	| "CenterParent"
+	| "CenterScreen"
+	| "Manual"
+	| "WindowsDefaultBounds"
+	| "WindowsDefaultLocation";
+
 export class Popup {
 	#code: {
 		addTypes: string;
@@ -175,10 +183,32 @@ export class Popup {
 
 	/**
 	 * Create a new `Popup` instance.
+	 *
+	 * This is used to make a fully customizable popup.
+	 *
+	 * Example of usage:
+	 * ```js
+	 * const info = new popup.Popup(); // We are not chaining from here, but you can if you want.
+	 *
+	 * info.createWindow("Server Reboot", [275, 175], "C:/Users/Someone/Popup-Prompt/favicon.ico")
+	 * 	.componentLabel(
+	 * 		"information",
+	 * 		"The server rebooted at 2:14 PM on 16/10/2021.",
+	 * 		[10, 20],
+	 * 		[280, 20]
+	 * 	)
+	 * 	.componentButton("button", "OK", [85, 75], [75, 25], "OK", true)
+	 * 	.renderWindow(true)
+	 * 	.openPopup((data, err) => {
+	 * 		if (err) {
+	 * 			console.log(err);
+	 * 		} else {
+	 * 			console.log(data);
+	 * 		}
+	 * 	});
+	 * ```
 	 */
-	constructor() {
-		this.#code;
-	}
+	constructor();
 
 	/**
 	 * Concat all of the code in `this.#code`.
@@ -200,4 +230,125 @@ export class Popup {
 		script: string,
 		callback: (data: string, err: string) => void
 	): void;
+
+	/**
+	 * This method creates a window, which is where all of the popup components go.
+	 *
+	 * _Note: This method does not open the popup/window. To do this, run `openPopup`._
+	 *
+	 * ### Notes for Icons!
+	 *
+	 * _Note: The path must be from the drive. Otherwise, the icon will not display (e.g. `C:/Users/Someone/Popup-Prompt/favicon.ico`, not `./favicon.ico`). This bug will soon be fixed._
+	 *
+	 * The tipical sizes for an icon are _16 × 16_, _32 × 32_, and _48 × 48_ pixels. For great icons, see [this website](https://icon-icons.com/)!
+	 *
+	 * @param title A title, which is the main title of the popup.
+	 * @param size The size, which is an array of two numbers. These numbers determine the size of the window. So, `[300, 200]` means that the width is `300`, and the height is `200`.
+	 * @param iconPath The icon path for the icon on the popup window. This must be an `.ico` picture extension. If the icon is empty, then the default is a picture will red, blue, and yellow blocks.
+	 * @param startPos the start position. This is the position of the window. This defaults to `CenterScreen`.
+	 * @param name The name of the window. **It is not recommended to change this value**, since if you do change it, then you will have to pass it in almost every other method. Therefore, it is easier to leave it alone (the default value is `window`).
+	 */
+	createWindow(
+		title: string,
+		size: [number, number],
+		iconPath?: string,
+		startPos?: startPos,
+		name?: string
+	): this;
+
+	/**
+	 * This method creates a component label in the popup.
+	 *
+	 * _Note: This method does not open the popup/window. To do this, run `openPopup`._
+	 *
+	 * @param name A name, which is like a variable for the label. **If you pass in a value that already exists, there can be errors.**
+	 * @param text The text, which is basically the text to display on the label.
+	 * @param location The location. This is an array of two numbers. These numbers determine the location from the top-left corner of the popup text, from the top-left of the popup window. For example, if the numbers are `[10, 20]`, then `10` is the width, and `20` is the height.
+	 * @param size The size. This is an array of two numbers. These numbers determine the size of the text. For example, if the numbers are `[300, 20]`, then `300` is the width, and `20` is the height.
+	 * @param windowName The name of the window. **It is not recommended to change this value**, since if you do change it, then you will have to pass it in almost every other method. Therefore, it is easier to leave it alone (the default value is `window`).
+	 */
+	componentLabel(
+		name: string,
+		text: string,
+		location: [number, number],
+		size: [number, number],
+		windowName?: string
+	): this;
+	/**
+	 * Create a component button in the popup.
+	 *
+	 * _Note: This method does not open the popup/window. To do this, run `openPopup`._
+	 *
+	 * @param name The name, which is the variable for the button. **Beware that if you pass a value that exists, there could be errors.**
+	 * @param text The text to display on the button.
+	 * @param location The location. This is an array of two numbers. These numbers determine the location from the top-left corner of the popup text, from the top-left of the popup window. For example, if the numbers are `[10, 20]`, then `10` is the width, and `20` is the height.
+	 * @param size The size. This is an array of two numbers. These numbers determine the size of the button. For example, if the numbers are `[75, 20]`, then `300` is the width, and `20` is the height.
+	 * @param result The result, which is the result of the button (if `listen` is true). This is what is returned if `listen` is true.
+	 * @param listen A listener. This will wait for a click event, and when it occurs, it will return the `result` value in `openPopup`.
+	 * @param windowName The name of the window. **It is not recommended to change this value**, since if you do change it, then you will have to pass it in almost every other method. Therefore, it is easier to leave it alone (the default value is `window`).
+	 */
+	componentButton(
+		name: string,
+		text: string,
+		location: [number, number],
+		size: [number, number],
+		result: string,
+		listen?: boolean,
+		windowName?: string
+	): this;
+	/**
+	 *
+	 * @param name A name, which is the text box's variable name. **Beware that if you pass a value that exists, there could be errors.**
+	 * @param location The location. This is an array of two numbers. These numbers determine the location from the top-left corner of the popup text, from the top-left of the popup window. For example, if the numbers are `[10, 20]`, then `10` is the width, and `20` is the height.
+	 * @param size The size. This is an array of two numbers. These numbers determine the size of the text box. For example, if the numbers are `[250, 20]`, then `250` is the width, and `20` is the height.
+	 * @param listen A listener. This will wait for a _KeyUp_ event, and when it occurs, it will return the text in `openPopup`. _Note: This method does not return the text after each KeyUp event. Also, beware that there could be **null** readings at times._
+	 * @param windowName The name of the window. **It is not recommended to change this value**, since if you do change it, then you will have to pass it in almost every other method. Therefore, it is easier to leave it alone (the default value is `window`).
+	 */
+	componentTextBox(
+		name: string,
+		location: [number, number],
+		size: [number, number],
+		listen?: boolean,
+		windowName?: string
+	): this;
+
+	/**
+	 * This method creates the code for the window to be properly rendered.
+	 *
+	 * _Note: This method does not open the popup/window. To do this, run `openPopup`._
+	 *
+	 * All of the parameters of this method are optional. Therefore, it is okay to not pass in anything.
+	 *
+	 * @param topmost If the window should be the topmost. This means that even if the window is out of focus, the popup will still stay on the top (unless the user clicks the minimize button).
+	 * @param name The name of the window. **It is not recommended to change this value**, since if you do change it, then you will have to pass it in almost every other method. Therefore, it is easier to leave it alone (the default value is `window`).
+	 */
+	renderWindow(topmost?: boolean, name?: string): this;
+
+	/**
+	 * This method opens the popup on the users computer.
+	 *
+	 * There is only one required parameter, and that is the callback. It is usually called as below.
+	 *
+	 * ```js
+	 * popup.openPopup((data, err) => {
+	 * 	if (err) {
+	 * 		console.log(err);
+	 * 	} else {
+	 * 		// Do whatever you want with the data returned.
+	 * 		console.log(data);
+	 * 	}
+	 * });
+	 * ```
+	 *
+	 * ### Callback Notes
+	 *
+	 * The first parameter that the callback passes is the data. This is the data from a button or text box (if `listen` is set to true). If there are no listeners, then the data will be a zero-length string. Otherwise, it will be the button clicked, or the text in the text box entered, depending on what has been listened.
+	 *
+	 * The second parameter that the callback passes is the error. This is a string telling the user that an error occured. _Note: This method does not exactly tell what the error is. This may be introduced in version 1.1.0._
+	 *
+	 * Once both of the parameters are there, then, in the callback body, you should add an `if/else` statement, and check if there is an error. The reason is because if there is an error, it will have a value, otherwise, it will have a value of `null`. The same applies for the `data`. In the `else` clause, you can do whatever you wish with the data.
+	 *
+	 * @param callback The callback to invoke when the callback has finished (or started, if there is an error).
+	 */
+	openPopup(callback: (data: string, err: string) => void): void;
 }
